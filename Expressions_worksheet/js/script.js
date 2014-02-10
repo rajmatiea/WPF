a9384ee266c204fb9a5c976b8e4e72e6f8dd00a8
rajmatiearjune <stephanie@graphicsflo.com>
2014-02-09 19:30:15 -0500

    Expression_worksheet

---
+++ Expressions_worksheet/js/script.js
@@ -0,0 +1,12 @@
+//Rajmatie Arjune, February 9, 2014: Expressions Worksheet
+
+//Calculate area of rectangle
+
+var width = 7;
+var height = 4;
+
+//Area of rectangle is width times height times two
+
+var area = width * height*2;
+console.log(area);
+
