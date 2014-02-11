a549568adf514d84cb66a2bee1715c8b11217465
rajmatiearjune <stephanie@graphicsflo.com>
2014-02-10 20:15:11 -0500

    Defining variable

--- Expressions_Wacky/js/script.js
+++ Expressions_Wacky/js/script.js
@@ -4,3 +4,8 @@
 //Teachers are to fill out the resourse list to determine the total cost per child
 
 var kids = prompt("How many kids are signed up for the trip?");
+
+var busRide = 1200; //bus day rate
+var lunch = 14; //per child
+var flags = 3.20; //flag as identitifier so no one gets lost
+var blanket = 5; //to stay warm
