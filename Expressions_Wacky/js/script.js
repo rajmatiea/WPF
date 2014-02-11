e203a2cb2c3f992130297ee14e46b9be4fa444f3
rajmatiearjune <stephanie@graphicsflo.com>
2014-02-10 20:04:08 -0500

    Creating variable for wacky expressions

--- Expressions_Wacky/js/script.js
+++ Expressions_Wacky/js/script.js
@@ -1,1 +1,6 @@
 //Rajmatie Arjune, February 10, 2014: Expressions Wacky
+
+//Planning class trip for 6th graders
+//Teachers are to fill out the resourse list to determine the total cost per child
+
+var kids = prompt("How many kids are signed up for the trip?");
