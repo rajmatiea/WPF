74809b3c30166ac58dfd6fa3d7b47713a1b01453
rajmatiearjune <stephanie@graphicsflo.com>
2014-02-10 22:15:38 -0500

    Updating expression

--- Expressions_Personal/js/script.js
+++ Expressions_Personal/js/script.js
@@ -1,7 +1,7 @@
-//Rajmatie Arjune, February 9, 2014: Expressions Personal
+//Rajmatie Arjune, February 10, 2014: Expressions Personal
 
 //Flying overseas on Tuesday early morning, but the local flight was mistakenly booked for the wrong date.
-//Six people were travelling, therefore upgrade to Monday is inevitable with price change.
+//Six people are travelling, therefore upgrade to Monday is inevitable with price change.
 
 var jetBlue = 375;
 var upgrade = 189;
@@ -10,6 +10,10 @@ var people = 6;
 var newPrice = jetBlue + upgrade;
 var total = newPrice * people;
 
-console.log("The price change for each person is $" + newPrice + ", and the total cost is $" + total);
+console.log("The price change for each person was $" + newPrice +",\n and the total cost was $" + total);
+alert("The price change for each person was $" + newPrice +",\n and the total cost was $" + total);
+
+
+
 
 
