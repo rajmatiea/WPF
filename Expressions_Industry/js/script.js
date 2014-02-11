718846a55483a47781268ab3fe03c7b824884a9a
rajmatiearjune <stephanie@graphicsflo.com>
2014-02-10 19:57:28 -0500

    Expressions Industry

--- Expressions_Industry/js/script.js
+++ Expressions_Industry/js/script.js
@@ -1,2 +1,19 @@
 //Rajmatie Arjune, February 9, 2014: Expressions Industry
 
+//Retirement planning for the year 2014.
+//Employee and employer contribution.
+
+var months = 12;
+var contribution = 0; //this value to be filled in by contributor
+
+var userName = prompt("What is your Name?");
+var company = prompt("What is your company name?");
+var contAmount = prompt("What amount in dollars do you contribute per month?")
+var employerContri = 15; //employee's contribution is 15%
+
+//Based upon employee's input their yearly earings would be printed to the console and displayed in the alert.
+alert(userName +" earnings from employer for the year is $" + employerContri/100 * contAmount * months);
+alert("Earnings from your contribution for the year is $" + contAmount * months);
+
+console.log("Earnings from employer for the year is $" + employerContri/100 * contAmount * months);
+console.log("Earnings from your contribution for the year is $" + contAmount * months);
