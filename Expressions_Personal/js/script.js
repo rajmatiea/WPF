374b0432c3d4932f713200b4d2bdd1d6d4ee4dcc
rajmatiearjune <stephanie@graphicsflo.com>
2014-02-11 20:29:52 -0500

    Adding variable for personal expression

--- Expressions_Personal/js/script.js
+++ Expressions_Personal/js/script.js
@@ -1,19 +1,26 @@
-//Rajmatie Arjune, February 10, 2014: Expressions Personal
+//Rajmatie Arjune, February 11, 2014: Expressions Personal
 
-//Flying overseas on Tuesday early morning, but the local flight was mistakenly booked for the wrong date.
-//Six people are travelling, therefore upgrade to Monday is inevitable with price change.
+//How much would you spend on coffee and creamer for the year.
 
-var jetBlue = 375;
-var upgrade = 189;
-var people = 6;
-
-var newPrice = jetBlue + upgrade;
-var total = newPrice * people;
-
-console.log("The price change for each person was $" + newPrice +",\n and the total cost was $" + total);
-alert("The price change for each person was $" + newPrice +",\n and the total cost was $" + total);
+var coffee = 4.75;  //per lb 
+var creamer = 3.50; //per bottle
 
 
 
 
 
+
+
+////Flying overseas on Tuesday early morning, but the local flight was mistakenly booked for the wrong date.
+////Six people are travelling, therefore upgrade to Monday is inevitable with price change.
+//
+//var jetBlue = 375;
+//var upgrade = 189;
+//var people = 6;
+//
+//var newPrice = jetBlue + upgrade;
+//var total = newPrice * people;
+//
+//console.log("The price change for each person was $" + newPrice +",\n and the total cost was $" + total);
+//alert("The price change for each person was $" + newPrice +",\n and the total cost was $" + total);
+//
