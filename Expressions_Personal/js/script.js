d47996e422a167945c0737dff64b10394c46f795
rajmatiearjune <stephanie@graphicsflo.com>
2014-02-11 22:20:31 -0500

    Comments added

--- Expressions_Personal/js/script.js
+++ Expressions_Personal/js/script.js
@@ -1,6 +1,7 @@
 //Rajmatie Arjune, February 11, 2014: Expressions Personal
 
-//How much would you spend on coffee and creamer for the year.
+//How much would you spend on coffee and creamer for the year?
+//You could consider cheaper option that is available.
 
 var coffee = 4.75;  //per lb 
 var creamer = 3.50; //per bottle
@@ -26,23 +27,3 @@ alert("If you buy from Zak's you will spend a total of $" + zakTotal * month);
 
 confirm("Thank you for your order! \nHave a nice day!");
 
-
-
-
-
-
-
-
-////Flying overseas on Tuesday early morning, but the local flight was mistakenly booked for the wrong date.
-////Six people are travelling, therefore upgrade to Monday is inevitable with price change.
-//
-//var jetBlue = 375;
-//var upgrade = 189;
-//var people = 6;
-//
-//var newPrice = jetBlue + upgrade;
-//var total = newPrice * people;
-//
-//console.log("The price change for each person was $" + newPrice +",\n and the total cost was $" + total);
-//alert("The price change for each person was $" + newPrice +",\n and the total cost was $" + total);
-//
