ee459c5a0a361962e4e493a9b75c40ebecb56911
rajmatiearjune <stephanie@graphicsflo.com>
2014-02-17 18:06:09 -0500

    Defining variable for conditional statement.

--- Conditionals_Personal/js/script.js
+++ Conditionals_Personal/js/script.js
@@ -1,1 +1,10 @@
 //Rajmatie Arjune, February 16, 2014, Conditionals Personal.
+
+//Conditional personal
+
+//Students are making brownies in their cooking class.
+
+var brownieMix = 2;
+var eggs = 2;
+var fudge = eggs * 1;
+var cakeBrownie = eggs * 3;
