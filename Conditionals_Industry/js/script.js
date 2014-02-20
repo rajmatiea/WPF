be4d536246615191fb21566c30e2e60531f1778d
rajmatiearjune <stephanie@graphicsflo.com>
2014-02-19 20:44:28 -0500

    Introduce variable.

--- Conditionals_Industry/js/script.js
+++ Conditionals_Industry/js/script.js
@@ -5,3 +5,9 @@
 
 //Employee data to be entered by each manager, this would give the basis for end result.
 var employee = prompt("Please enter the amount of employees in your Department.")
+
+var memoPages = 5;
+var manager = 1;
+var paper = .019;
+var ink = .065;
+
