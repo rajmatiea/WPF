8efe0cd04c37e6aedd6c9815f242affcb93d5341
rajmatiearjune <stephanie@graphicsflo.com>
2014-02-19 20:42:35 -0500

    Prompt message for user data input.

--- Conditionals_Industry/js/script.js
+++ Conditionals_Industry/js/script.js
@@ -2,3 +2,6 @@
 
 //Company Director working to save resources, and has to determine whether to print memo,
 //or emailing to staff is cost effective.
+
+//Employee data to be entered by each manager, this would give the basis for end result.
+var employee = prompt("Please enter the amount of employees in your Department.")
