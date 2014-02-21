cec8edba7579a6e524ce19d79dd41b3b6fa22a62
rajmatiearjune <stephanie@graphicsflo.com>
2014-02-20 19:19:24 -0500

    Adding variable for wacky condition.

--- Conditionals_Wacky/js/script.js
+++ Conditionals_Wacky/js/script.js
@@ -1,1 +1,10 @@
-//Rajmatie Arjune, February 16, 2014, Conditionals Personal.
+//Rajmatie Arjune, February 19, 2014, Conditionals Wacky.
+
+//Ten friends pooling to buy gift/gifts for their friend.
+//If they collect amoungst themselves a total of $500, they will buy a guitar and camera.
+
+var targetAmount = 500;
+var friend = 10;
+var camera = 255;
+var guitar = 205;
+
