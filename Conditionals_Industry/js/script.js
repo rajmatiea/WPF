5a1149d71e33aa52f67865d550c35e0f3780269b
rajmatiearjune <stephanie@graphicsflo.com>
2014-02-23 17:43:58 -0500

    collecting information from prompt, if not exactly the same error message will display

--- Conditionals_Industry/js/script.js
+++ Conditionals_Industry/js/script.js
@@ -4,6 +4,19 @@
 //or emailing to staff is cost effective.
 
 //Employee data to be entered by each manager, this would give the basis for end result.
+//collecting information from prompt, if not exactly the same error message.
+var director = 27554;
+var employeeNum = prompt("Please enter your Employee Number.");
+//employee number to be entered as in the defined variable.
+if (employeeNum < 27554) {
+    console.log("Sorry your entry is incorrect, please try again.");
+    prompt("Sorry your entry is incorrect, please try again.");
+    //code
+}else{
+    console.log("Thank you Mr. Smith for your entry!");
+    alert("Thank you Mr. Smith for your entry!");
+}
+
 var employee = prompt("Please enter the amount of employees in your Department.")
 
 var memoPages = 5;
