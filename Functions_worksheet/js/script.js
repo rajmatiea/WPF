d7aa56215c6ef3cda61f13420694c09be6e8c6aa
rajmatiearjune <stephanie@graphicsflo.com>
2014-02-23 12:44:41 -0500

    commenting what each line of codes are doing

--- Functions_worksheet/js/script.js
+++ Functions_worksheet/js/script.js
@@ -3,16 +3,22 @@
 //Circumference
 //Calculate the circumference of a circle
 
-function circleCircum(r, d){
+//function parameters
+function circleCircum(r, d) {
+//variable declaration
     var r = 30;
-    
     var d = 25;
     
+//calculating the area of the cirle     
     var circumfrence = r * d;
-    //return circumfrence;
+    //print to console
     console.log("The circumference of the circle is " + circumfrence);
-
+    //returning value
+    return circumfrence;
 }
-
+//function call
 circleCircum();
 
+
+
+//Stung!
