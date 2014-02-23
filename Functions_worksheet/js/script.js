2f9e48e10aa395736d6ee7b044d32a2cbf2988e4
rajmatiearjune <stephanie@graphicsflo.com>
2014-02-23 12:06:42 -0500

    calculating the circumference of a circle

--- Functions_worksheet/js/script.js
+++ Functions_worksheet/js/script.js
@@ -1,1 +1,18 @@
 //Rajmatie Arjune, February 23, 2014, Functions_worksheet
+
+//Circumference
+//Calculate the circumference of a circle
+
+function circleCircum(r, d){
+    var r = 30;
+    
+    var d = 25;
+    
+    var circumfrence = r * d;
+    //return circumfrence;
+    console.log("The circumference of the circle is " + circumfrence);
+
+}
+
+circleCircum();
+
