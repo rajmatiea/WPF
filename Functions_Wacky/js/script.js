075cd4e56b99381a3060cb3decb77487ccf68583
rajmatiearjune <stephanie@graphicsflo.com>
2014-02-25 21:03:04 -0500

    adding prompts to collect comparing information from user

--- Functions_Wacky/js/script.js
+++ Functions_Wacky/js/script.js
@@ -6,9 +6,18 @@
     var online = 16;
     var shipping = 5;
     var onlinePurchase = online + shipping;
-    var inStore = prompt("what is the listed price in the store for the book?");
     var result = onlinePurchase - inStore;
 
     var walMart = 18;
     var barnesAndNoble = 19;
     var bookAMillion = 20;
+    
+function myBookOrder(onlinePurchase, inStore) {
+    //prompts for user entry
+    var userEntry = prompt("What price did you want to pay?");
+    var inStore = prompt("what is the listed price in the store for the book?");
+    
+    
+    
+    
+}
