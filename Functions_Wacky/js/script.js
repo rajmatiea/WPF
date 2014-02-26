f0d17365449d1d30ff093c487e6f16f80bbc90b5
rajmatiearjune <stephanie@graphicsflo.com>
2014-02-25 21:06:42 -0500

    adding comparable operator to calculate user input

--- Functions_Wacky/js/script.js
+++ Functions_Wacky/js/script.js
@@ -6,6 +6,7 @@
     var online = 16;
     var shipping = 5;
     var onlinePurchase = online + shipping;
+    var inStore = prompt("what is the listed price in the store for the book?");
     var result = onlinePurchase - inStore;
 
     var walMart = 18;
@@ -15,9 +16,12 @@
 function myBookOrder(onlinePurchase, inStore) {
     //prompts for user entry
     var userEntry = prompt("What price did you want to pay?");
-    var inStore = prompt("what is the listed price in the store for the book?");
+    
+    if (userEntry < inStore != userEntry < result) {
+        alert("You could buy online for $14 plus $5 shippping and handling.");
+        console.log("You could buy online for $14 plus $5 shippping and handling.");
     
     
-    
+    }
     
 }
