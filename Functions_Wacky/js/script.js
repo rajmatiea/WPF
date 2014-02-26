c51079be396704924b8bed1a6086deae09d21f19
rajmatiearjune <stephanie@graphicsflo.com>
2014-02-25 20:54:41 -0500

    adding variable

--- Functions_Wacky/js/script.js
+++ Functions_Wacky/js/script.js
@@ -1,1 +1,14 @@
 //Rajmatie Arjune, February 23, Functions_Wacky
+
+//Shopping online and in store for a motivational book.
+
+//online you could pay one price, but here we'll compare once we check the price in store vs.online.
+    var online = 16;
+    var shipping = 5;
+    var onlinePurchase = online + shipping;
+    var inStore = prompt("what is the listed price in the store for the book?");
+    var result = onlinePurchase - inStore;
+
+    var walMart = 18;
+    var barnesAndNoble = 19;
+    var bookAMillion = 20;
