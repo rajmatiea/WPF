0171ae18f992878e5418f31d052605eb6299194c
rajmatiearjune <stephanie@graphicsflo.com>
2014-02-26 20:39:01 -0500

    starting function with items and prices

--- Functions_Personal/js/script.js
+++ Functions_Personal/js/script.js
@@ -1,1 +1,14 @@
-//Rajmatie Arjune, February 23, Functions Personal
+//Rajmatie Arjune, February 26, Functions Personal
+
+//Shopping online for clothes and shoe.
+
+//Welcoming visitor to the online store.
+var clothesShopping = function(){
+    alert("Welcome to the Store!");
+
+//Items and prices    
+    var dress = 15.50;
+    var jeans = 22.95;
+    var blouse = 17.25;
+    var shoes = 35.99;
+    var shipping = 25;
