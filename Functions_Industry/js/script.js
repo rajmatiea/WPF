e01d5a0548663069f6efae6b413eb25d881f5bfb
rajmatiearjune <stephanie@graphicsflo.com>
2014-02-26 21:35:49 -0500

    adding if, else if, else statement

--- Functions_Industry/js/script.js
+++ Functions_Industry/js/script.js
@@ -6,12 +6,24 @@ function mySpreadsheet() {
 //Cells for spreadsheet would be determined based upon input.
 
     var cells = 0;
-    var width = 15;
-    var height = 11;
+    var width = prompt("What is the width of your spreadsheet?");
+    var height = prompt("What is the height of your spreadsheet?");
     
     var dimension = width * height;
-    console.log("You will have a spreadsheet with " + dimension + "cells.");
+    console.log("You will have a spreadsheet with " + dimension + " cells.");
+    
+    if (width < 14) {
+        alert("You could try to use a smaller size spreadsheet to do your statement.");
+        
+    }else if (height > 12) {
+        alert("You will need to switch to portrait size to fit your statement.");
+        
+    }else{
+        alert("You will have a spreadsheet with " + dimension + " cells.");
+    }
 
     return dimension;
 
-    
+    }
+
+mySpreadsheet()
