a4188d86cca2c9fe4c64b9a8e084452f8a40d81c
rajmatiearjune <stephanie@graphicsflo.com>
2014-02-26 20:45:32 -0500

    Commenting line items

--- Functions_Personal/js/script.js
+++ Functions_Personal/js/script.js
@@ -12,13 +12,15 @@ var clothesShopping = function(){
     var blouse = 17.25;
     var shoes = 35.99;
     var shipping = 25;
-    
+//You have the option to buy these items.     
     var subTotal = [dress + jeans + blouse + shoes];
     
+//Enter the quantity of items as prompted.    
     var customer = prompt("Please enter quantity of dress needed.");
     var customerOne = prompt("Please enter quantity of jeans needed.");
     var customerTwo = prompt("Please enter quantity of blouse needed.");
     var customerThree = prompt("Please enter quantity of shoes needed.");
-    
+
+//The total cost is calculated with shipping charge added.    
     var totaltest = subTotal + shipping;
     var total = subTotal * customer + shipping;
