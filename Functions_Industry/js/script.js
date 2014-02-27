c722c96900de7a96ee84298dfcf749aa31bc6dfe
rajmatiearjune <stephanie@graphicsflo.com>
2014-02-26 21:13:21 -0500

    adding function statement and variable

--- Functions_Industry/js/script.js
+++ Functions_Industry/js/script.js
@@ -1,1 +1,17 @@
-//Rajmatie Arjune, February 23, Functions_Industry
+//Rajmatie Arjune, February 26, Functions_Industry.
+
+//Preparing spreadsheet for income/expenditure statement.
+
+function mySpreadsheet() {
+//Cells for spreadsheet would be determined based upon input.
+
+    var cells = 0;
+    var width = 15;
+    var height = 11;
+    
+    var dimension = width * height;
+    console.log("You will have a spreadsheet with " + dimension + "cells.");
+
+    return dimension;
+
+    
