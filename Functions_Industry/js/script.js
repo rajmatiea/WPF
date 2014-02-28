00adc929f4c673a73572f64729736730d53af08c
rajmatiearjune <stephanie@graphicsflo.com>
2014-02-27 21:30:27 -0500

    comments added for action being done

--- Functions_Industry/js/script.js
+++ Functions_Industry/js/script.js
@@ -14,17 +14,21 @@ function mySpreadsheet(cells,width,height) {
     var width = prompt("What is the width of your spreadsheet?");
     var height = prompt("What is the height of your spreadsheet?");
     
+//dimension for width and height entry.    
     var dimension = width * height;
     console.log("You will have a spreadsheet with " + dimension + " cells.");
     
+//If width is less than or equal to 11 and height greater than or equal to 10, choose smaller size.     
     if (width <= 11 && height >= 10) {
         alert("You could try to use a smaller size spreadsheet to do your statement.");
         console.log("You could try to use a smaller size spreadsheet to do your statement.");
         
+//Else if height is exactly 12 or width is excactly 7, switch to portrait size spreadsheet.           
     }else if (height === 12 || width === 7) {
         alert("You will need to switch to portrait size to fit your statement.");
         console.log("You will need to switch to portrait size to fit your statement.");
         
+//Else you could use the template available in the office.                        
     }else{
         alert("You could use the spreadsheet template " + dimension + " cells.");
         console.log("You could use the office spreadsheet template " + dimension + " cells.");
