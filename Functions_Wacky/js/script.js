75ad66e286539c21f521ebac60f1ed19e7e6bedd
rajmatiearjune <stephanie@graphicsflo.com>
2014-02-27 21:23:02 -0500

    adding parameters executable in function

--- Functions_Wacky/js/script.js
+++ Functions_Wacky/js/script.js
@@ -2,26 +2,26 @@
 
 //Shopping online and in store for a motivational book.
 //online you could pay one price, but here we'll compare once we check the price in store vs.online.
-    
-    var online = 16;
-    var shipping = 5;
-    var onlinePurchase = online + shipping;
-    var inStore = prompt("what is the listed price in the store for the book?");
-    var result = onlinePurchase - inStore;
-
-    var walMart = 18;
-    var barnesAndNoble = 19;
-    var bookAMillion = 20;
-    
+     
 function myBookOrder(onlinePurchase, inStore) {
-    //prompts for user entry
-    var userEntry = prompt("What price dos you want to pay?");
+    //prompts for user to tell us what price they are looking to pay.
+    var userEntry = prompt("What price are you looking to pay for the book?");
     
-    if (userEntry < inStore != userEntry < result) {
+    var online = 16; //Book price online.
+    var shipping = 5; //Shipping charge is an additional price.
+    var onlinePurchase = online + shipping; //Total price for online purchase.
+    
+    //Price user sees in store for the books.
+    var inStore = prompt("what is the listed price in the store for the book?");
+    
+    //Calculating comparing prices online vs store.
+    var result = onlinePurchase - inStore;
+    
+    if (userEntry < inStore != userEntry <= onlinePurchase) {
         alert("You could buy online for $14 plus $5 shippping and handling.");
         console.log("You could buy online for $14 plus $5 shippping and handling.");
         
-    }else if (userEntry > 20) {
+    }else if (userEntry < onlinePurchase) {
         alert("You could buy the book from Book-A-Million for $20.");
         console.log("You could buy the book from Book-A-Million for $20.");
     
