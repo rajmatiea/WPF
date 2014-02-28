b2fd604aaa7670d057dfaea179c448ff1b265d6b
rajmatiearjune <stephanie@graphicsflo.com>
2014-02-27 21:54:39 -0500

    comments added

--- Functions_Personal/js/script.js
+++ Functions_Personal/js/script.js
@@ -6,14 +6,15 @@
 var clothesShopping = function(){
     alert("Welcome to the Store!");
 
-//Items and prices    
+//These are the prices for our Items. Also there is a flat rate shipping charge.    
     var dress = 15.50;
     var jeans = 22.95;
     var blouse = 17.25;
     var shoes = 35.99;
     var shipping = 25;
-//You have the option to buy these items.     
-    var subTotal = [dress + jeans + blouse + shoes];
+    
+//These items would be added up based upon customer quantity.     
+    var subTotal = dress + jeans + blouse + shoes;
     
 //Enter the quantity of items as prompted.    
     var customer = prompt("Please enter quantity of dress needed.");
@@ -21,11 +22,11 @@ var clothesShopping = function(){
     var customerTwo = prompt("Please enter quantity of blouse needed.");
     var customerThree = prompt("Please enter quantity of shoes needed.");
 
-//The total cost is calculated with shipping charge added.    
+//Your total cost is calculated with shipping charge added.    
     var totaltest = subTotal + shipping;
     var total = subTotal * customer + shipping;
     
-//Result will be printed to console based upon user response.    
+//Your result will be printed to console as you respond. And total purchase would be displayed.    
     console.log("Your total order with shipping is $" + total);
     alert("Your total order with shipping is $" + total);
     confirm("Please confirm your total is correct!");
